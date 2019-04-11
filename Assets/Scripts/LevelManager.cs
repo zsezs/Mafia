cc8c7945c645d162166db2e9493ab707eb07c278
AssassinViper <amd.amp1969@gmail.com>
2019-04-11 22:31:20 +0430

    adding lib

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,17 +2,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using SocketIO;
 
 public class LevelManager : MonoBehaviour {
 
+	public SocketIOComponent socket;
+
+	public string room;
+
+	public void Start(){
+
+		GameObject go = GameObject.Find("SocketIO");
+    	
+		socket = go.GetComponent<SocketIOComponent>();
+		
+		socket.On("join_r", (SocketIOEvent e) => {
+			
+			room = e.room;
+
+			SceneManager.LoadScene("Chat");
+		});
+
+		socket.On("message_r", (SocketIOEvent e) => {
+
+			ShowMessage(e.name, e.message);
+		});
+	}
+
 	public void LoadLevel(string name){
-		Debug.Log ("New Level load: " + name);
-		SceneManager.LoadScene(name);
+
+		JoinRoom(name);
 	}
 
 	public void QuitRequest(){
 		Debug.Log ("Quit requested");
-		Application.Quit ();
+		Application.Quit();
 	}
 
+	public void JoinRoom(string name){
+
+		Dictionary<string, string> data = new Dictionary<string, string>();
+       	
+		data["name"] = name;
+
+       	socket.Emit("join", new JSONObject(data));
+	}
+
+	public void SendMessage(string name, string message, string room){
+
+		Dictionary<string, string> data = new Dictionary<string, string>();
+       	
+		data["name"] = name;
+		data["room"] = room;
+		data["message"] = message;
+
+		socket.Emit("message", new JSONObject(data));
+	}
+
+	public void ShowMessage(string name, string message){
+
+		//create new message card
+
+
+	}
 }
