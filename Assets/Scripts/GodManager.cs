13fa908bcc8e99be464c37ca87604e9eb3aefdf3
zsezs <alireza1376@gmail.com>
2019-04-17 15:27:26 +0430

    God Panel
    God Panel

---
+++ Assets/Scripts/GodManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GodManager : MonoBehaviour
+{
+
+    public GameObject godButton;
+    public GameObject godPanel;
+    
+    void Start()
+    {
+        godPanel.SetActive(false);
+    }
+    void Update()
+    {
+        
+    }
+
+    public void onClick()
+    {
+        godPanel.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/RoleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoleManager : MonoBehaviour
+{
+    public Text rollName ;
+    public GameObject rollPanel;
+
+    public void Awake()
+    {
+       // rollPanel = new GameObject();
+        rollPanel.SetActive(false);
+
+    }
+    public void Start()
+    {
+      
+    }
+
+    public void Update()
+    {
+        rollName.text = "Mafia";
+    }
+
+    public void onClick()
+    {
+        rollPanel.SetActive(true);
+    }
+
+}
